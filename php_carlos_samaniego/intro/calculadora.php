938262bf1020e25dee526fe6ba366597e67a9aac
sebastiansamaniegodp <138633176+sebastiansamaniegodp@users.noreply.github.com>
2023-09-19 21:29:50 +0200

    Add files via upload

---
+++ php_carlos_samaniego/intro/calculadora.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="style.css"/>
+<body>
+    <input type="text" id="numero1" placeholder="Numero1">
+    <input type="text" id="numero2" placeholder="Numero2">=
+    <input type="text" id="resultado" placeholder="Resultado">
+    <br>
+    <button id="sumar">+</button>
+    <button id="restar">-</button>
+    <button class="operador">/</button>
+    <button class="operador">*</button>
+    <button class="operador">X<sup>y</sup></button>
+</body>
