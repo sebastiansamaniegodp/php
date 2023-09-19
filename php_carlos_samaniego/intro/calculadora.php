d5165ceecbd0b8cea15a7ac3566e9c6b52431a0d
sebastiansamaniegodp <138633176+sebastiansamaniegodp@users.noreply.github.com>
2023-09-19 22:52:22 +0200

    Añadir lógica

--- php_carlos_samaniego/intro/calculadora.php
+++ php_carlos_samaniego/intro/calculadora.php
@@ -1,12 +1,64 @@
-<link rel="stylesheet" href="style.css"/>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora PHP</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
 <body>
-    <input type="text" id="numero1" placeholder="Numero1">
-    <input type="text" id="numero2" placeholder="Numero2">=
-    <input type="text" id="resultado" placeholder="Resultado">
-    <br>
-    <button id="sumar">+</button>
-    <button id="restar">-</button>
-    <button class="operador">/</button>
-    <button class="operador">*</button>
-    <button class="operador">X<sup>y</sup></button>
+    <h1>Calculadora PHP</h1>
+    <form method="post" action="">
+        <input type="number" name="numero1" placeholder="Numero1" required>
+        <input type="number" name="numero2" placeholder="Numero2" required>
+        <select name="operador">
+            <option value="sumar">Suma (+)</option>
+            <option value="restar">Resta (-)</option>
+            <option value="multiplicar">Multiplicacion (*)</option>
+            <option value="dividir">Division (/)</option>
+            <option value="cuadrado">Cuadrado (2)</option>
+        </select>
+        <input type="submit" value="Calcular">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numero1 = $_POST["numero1"];
+        $numero2 = $_POST["numero2"];
+        $operador = $_POST["operador"];
+        $resultado = 0;
+
+        switch ($operador) {
+            case "sumar":
+                $resultado = $numero1 + $numero2;
+                break;
+
+            case "restar":
+                $resultado = $numero1 - $numero2;
+                break;
+
+            case "multiplicar":
+                $resultado = $numero1 * $numero2;
+                break;
+
+            case "dividir":
+                if ($numero2 != 0) {
+                    $resultado = $numero1 / $numero2;
+                } else {
+                    echo "¡No se puede dividir entre cero!";
+                }
+                break;
+
+            case "cuadrado":
+                $resultado = $numero1 * $numero1;
+                break;
+
+            default:
+                echo "Operacion no valida.";
+                break;
+        }
+
+        echo "<p>Resultado: $resultado</p>";
+    }
+    ?>
+    <input type="text" value="<?php echo "<p>Resultado: $resultado</p>";?>">
 </body>
+</html>
