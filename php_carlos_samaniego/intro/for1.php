bc3f5b30baa13b8dc0f7116439515319606fc734
sebastiansamaniegodp <138633176+sebastiansamaniegodp@users.noreply.github.com>
2023-09-25 16:25:23 -0400

    Agregar título a las tablas de multiplicar

--- php_carlos_samaniego/intro/for1.php
+++ php_carlos_samaniego/intro/for1.php
@@ -9,6 +9,7 @@
         //imprimir una tabla de multiplicar con for()
         //luego mejorar para imprimir todas las tablas del 1 al 10
         for ($x = 1; $x <= 10; $x++) {
+            echo "Tabla del " . $x . "<br>";
             for ($y = 0; $y <= 10; $y++) {
                 echo "$x x $y = " .($x * $y) . "<br>";
                 if ($y == 10) {
