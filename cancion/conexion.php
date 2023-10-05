f04a717d095921a075dcac83b4daf65142ff4966
sebastiansamaniegodp <sebis.kek@gmail.com>
2023-10-05 13:55:29 -0300

    Remover mensajes.

--- cancion/conexion.php
+++ cancion/conexion.php
@@ -2,9 +2,5 @@
     include "vars.php";
 
     $conexion = new mysqli($host,$user,$password,$database, $port);
-
-    if($conexion->connect_errno){
-        echo "No se pudo conectar ".$conexion->connect_error;
-    }
-    echo "Conexion OK".$conexion->host_info;
+    //crear conexion
 ?>
